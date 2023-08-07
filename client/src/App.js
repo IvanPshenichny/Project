90793aa14bab21860ae1d3dfe6a73c1ee707f584
IvanPshenichny <122299972+IvanPshenichny@users.noreply.github.com>
2023-08-07 20:12:41 +0300

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,75 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import Register from "./components/Register";
+import { ToDo } from "./components/ToDoList";
+import { Users } from "./components/Users";
+import { Calculator } from "./components/calculator/Calculator";
+
+function App() {
+  const [isAuth, setAuth] = useState(false);
+  const setAuthentification = (boolean) => {
+    setAuth(boolean);
+  };
+  async function isauth() {
+    try {
+      const response = await fetch("http://localhost:3001/auth/is-verify", {
+        method: "GET",
+        headers: { token: localStorage.token },
+      });
+      const parseRes = await response.json()
+      parseRes===true ? setAuth(true) : setAuth(false)
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+  useEffect(() => {
+    isauth();
+  });
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            !isAuth ? (
+              <Login setAuthentification={setAuthentification} />
+            ) : (
+              <Navigate to="/dashboard" />
+            )
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            !isAuth ? (
+              <Register setAuthentification={setAuthentification} />
+            ) : (
+              <Navigate to="/dashboard" />
+            )
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            isAuth ? (
+              <Dashboard setAuthentification={setAuthentification} />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        >
+          <Route path="Users" element={<Users />} />
+          <Route path="ToDoList" element={<ToDo />} />
+          <Route path="Calculator" element={<Calculator />} />
+        </Route>
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
