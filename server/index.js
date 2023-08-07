379d8a752ce2d04d8b72ff2bdd30ff63daf06a0b
IvanPshenichny <122299972+IvanPshenichny@users.noreply.github.com>
2023-08-07 19:48:55 +0300

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const jwtAuth = require("./Routes/jwtAuth");
+const dashboard = require("./Routes/dashboard");
+const allUsers = require("./Routes/allUsers");
+const deleteUsers = require("./Routes/deleteUsers");
+const PORT = 3001;
+
+app.use(express.json()); //req.body
+app.use(cors());
+
+// register and login routes
+
+app.use("/auth/", jwtAuth);
+app.use("/dashboard", dashboard);
+app.use("/allUsers", allUsers);
+app.use("/deleteUsers/:id", deleteUsers);
+
+const start = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`server has been started on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+start();
